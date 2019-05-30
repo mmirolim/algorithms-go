797c1c562e28db6677b9ac499d4c3e72fb8087cf
Mirolim Mirzakhmedov <mirolim.mirzakhmedov@gmail.com>
2019-05-31 01:27:22 +0500

    add Rabin-Karp sub string search

--- strings/strings_test.go
+++ strings/strings_test.go
@@ -4,6 +4,37 @@ import (
 	"testing"
 )
 
+func TestRabinKarpSubStringSearch(t *testing.T) {
+	prime := 619
+	for i, data := range []struct {
+		Str, Pat string
+		expected []int
+	}{
+		{"", "", []int{0}},
+		{"AABAACAADAABAABA", "AABA", []int{0, 9, 12}},
+		{"ALKJFDF", "ZAB", []int{-1}},
+	} {
+		out := RabinKarpSubStringSearch(data.Pat, data.Str, prime)
+		if len(out) != len(data.expected) {
+			t.Errorf("case [%d] expected %v, got %v", i, data.expected, out)
+			continue
+		}
+		for _, j := range out {
+			if j == -1 && data.expected[0] == -1 {
+				break
+			}
+
+			subStr := string(data.Str[j : j+len(data.Pat)])
+			if subStr != data.Pat {
+				t.Errorf("case [%d] expected %+v, got %v \n",
+					i, data.expected, out) // output for debug
+				break
+			}
+		}
+	}
+
+}
+
 func TestSearchMinWindow(t *testing.T) {
 	for i, data := range []struct {
 		Str, Pat, expected string
--- strings/sub-string.go
+++ strings/sub-string.go
@@ -1,5 +1,53 @@
 package strings
 
+func RabinKarpSubStringSearch(pat, str string, prime int) []int {
+	var out []int
+	if len(pat) > len(str) {
+		return []int{-1}
+	}
+	h := 1
+	const MAXCHAR = 256 // ASCII ext
+
+	// compute alpha^(M-1) mod prime
+	for i := 0; i < len(pat)-1; i++ {
+		h = (h * MAXCHAR) % prime
+	}
+	patHash, subStrHash := 0, 0
+
+	for i := range pat {
+		patHash = (MAXCHAR*patHash + int(pat[i])) % prime
+		subStrHash = (MAXCHAR*subStrHash + int(str[i])) % prime
+	}
+
+	for i := 0; i <= len(str)-len(pat); i++ {
+		if patHash == subStrHash {
+			// verify
+			found := true
+			for j := range pat {
+				if pat[j] != str[i+j] {
+					found = false
+					break
+				}
+			}
+			if found {
+				out = append(out, i)
+			}
+		}
+		if i < len(str)-len(pat) {
+			subStrHash =
+				(MAXCHAR*(subStrHash-h*int(str[i])) + int(str[i+len(pat)])) % prime
+			if subStrHash < 0 {
+				subStrHash += prime
+			}
+		}
+	}
+
+	if len(out) == 0 {
+		return []int{-1}
+	}
+	return out
+}
+
 func searchMinWindow(pat, str string) (index, minlen int) {
 	strDic := map[byte]int{}
 	patDic := map[byte]int{}
