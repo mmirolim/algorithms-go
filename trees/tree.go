410456cfd878abfcb9344c14c0b4f7bacc81ea9d
Mirolim Mirzakhmedov <mirolim.mirzakhmedov@gmail.com>
2019-05-30 23:40:44 +0500

    implement binary tree

--- trees/tree.go
+++ trees/tree.go
@@ -6,20 +6,108 @@ import (
 )
 
 type Tree struct {
-	root *Node
+	val                 int
+	parent, left, right *Tree
 }
 
-type Node struct {
-	val         int
-	left, right *Node
+func Delete(t **Tree, val int) {
+	if (*t).val == val {
+		// case leaf node
+		if (*t).left == nil && (*t).right == nil {
+			*t = nil
+			return
+		} else if (*t).right == nil {
+			// case one left node
+			(*t).left.parent = (*t).parent
+			if (*t).parent.left != nil && (*t).parent.left.val == (*t).val {
+				(*t).parent.left = (*t).left
+			} else {
+				(*t).parent.right = (*t).left
+			}
+		} else if (*t).left == nil {
+			// case one right node
+			(*t).right.parent = (*t).parent
+			if (*t).parent.left != nil && (*t).parent.left.val == (*t).val {
+				(*t).parent.left = (*t).right
+			} else {
+				(*t).parent.right = (*t).right
+
+			}
+		} else {
+			// case has subtree
+			min := (*t).right
+			for ; min.left != nil; min = min.left {
+			}
+			if min.right != nil {
+				// case one right node
+				min.right.parent = min.parent
+				min.parent.left = min.right
+			}
+			(*t).val = min.val
+			min.parent.left = nil
+		}
+	} else if (*t).val > val {
+		Delete(&(*t).left, val)
+	} else {
+		Delete(&(*t).right, val)
+	}
+}
+
+func Insert(t **Tree, val int) {
+	var insert func(t **Tree, p *Tree)
+	insert = func(t **Tree, p *Tree) {
+		if *t == nil {
+			*t = &Tree{
+				val:    val,
+				parent: p,
+				left:   nil,
+				right:  nil}
+		} else if (*t).val > val {
+			insert(&(*t).left, *t)
+		} else {
+			insert(&(*t).right, *t)
+		}
+	}
+
+	insert(t, nil)
+}
+
+func (t *Tree) FindMin() int {
+	min := t
+	for ; min.left != nil; min = min.left {
+	}
+
+	return min.val
+}
+
+func (t *Tree) FindMax() int {
+	max := t
+	for ; max.right != nil; max = max.right {
+	}
+
+	return max.val
+}
+
+func (t *Tree) Search(val int) bool {
+	if t == nil {
+		return false
+	}
+	if t.val == val {
+		return true
+	}
+
+	if t.val > val {
+		return t.right.Search(val)
+	}
+	return t.left.Search(val)
 }
 
 func (t *Tree) IsMirrorRecur(t2 *Tree) bool {
-	if t.root == nil && t2.root == nil {
+	if t == nil && t2 == nil {
 		return true
 	}
-	var isMirror func(n1, n2 *Node) bool
-	isMirror = func(n1, n2 *Node) bool {
+	var isMirror func(n1, n2 *Tree) bool
+	isMirror = func(n1, n2 *Tree) bool {
 		if n1 == nil && n2 == nil {
 			return true
 		}
@@ -30,24 +118,25 @@ func (t *Tree) IsMirrorRecur(t2 *Tree) bool {
 			isMirror(n1.left, n2.right) && isMirror(n1.right, n2.left)
 	}
 
-	return isMirror(t.root, t2.root)
+	return isMirror(t, t2)
 }
+
 func (t *Tree) IsMirror(t2 *Tree) bool {
-	if t.root == nil && t2.root == nil {
+	if t == nil && t2 == nil {
 		return true
 	}
 
 	tq1 := newQueue()
 	tq2 := newQueue()
 
-	t1node := t.root
-	t2node := t2.root
+	t1node := t
+	t2node := t2
 
 	for ; t1node != nil && t2node != nil; t1node, t2node = tq1.pop(), tq2.pop() {
 		if t1node.val != t2node.val {
 			return false
 		}
-		nodes := []*Node{t1node.left, t2node.right, t1node.right, t2node.left}
+		nodes := []*Tree{t1node.left, t2node.right, t1node.right, t2node.left}
 		for i := 0; i < len(nodes)-1; i += 2 {
 			if nodes[i] != nil && nodes[i+1] != nil {
 				tq1.push(nodes[i])
@@ -64,15 +153,15 @@ func (t *Tree) IsMirror(t2 *Tree) bool {
 }
 
 func (t *Tree) Reverse() {
-	if t == nil || t.root == nil {
+	if t == nil {
 		return
 	}
 
-	reverse := func(node *Node) {
+	reverse := func(node *Tree) {
 		node.left, node.right = node.right, node.left
 	}
 	qu := newQueue()
-	qu.push(t.root)
+	qu.push(t)
 	for node := qu.pop(); node != nil; node = qu.pop() {
 		if node.left != nil {
 			qu.push(node.left)
@@ -85,18 +174,18 @@ func (t *Tree) Reverse() {
 }
 
 type queue struct {
-	store []*Node
+	store []*Tree
 }
 
 func newQueue() *queue {
-	return &queue{store: []*Node{}}
+	return &queue{store: []*Tree{}}
 }
 
-func (q *queue) push(node *Node) {
+func (q *queue) push(node *Tree) {
 	q.store = append(q.store, node)
 }
 
-func (q *queue) pop() *Node {
+func (q *queue) pop() *Tree {
 	if len(q.store) == 0 {
 		return nil
 	}
@@ -105,13 +194,13 @@ func (q *queue) pop() *Node {
 	return node
 }
 
-func (t *Tree) String() string {
-	if t == nil || t.root == nil {
+func (t *Tree) ToString() string {
+	if t == nil {
 		return "empty"
 	}
 	queue := newQueue()
 	var str strings.Builder
-	queue.push(t.root)
+	queue.push(t)
 	for node := queue.pop(); node != nil; node = queue.pop() {
 		if node.left != nil {
 			queue.push(node.left)
--- trees/tree_test.go
+++ trees/tree_test.go
@@ -8,48 +8,140 @@ var treeMirrorTestData = []struct {
 	t1, t2 *Tree
 	out    bool
 }{
-	{t1: &Tree{root: nil}, t2: &Tree{root: nil}, out: true},
-	{t1: &Tree{root: &Node{val: 1,
-		left: &Node{val: 2,
+	{t1: nil, t2: nil, out: true},
+	{t1: &Tree{parent: nil,
+		left: &Tree{val: 2,
 			left:  nil,
 			right: nil},
-		right: &Node{val: 3,
+		right: &Tree{val: 3,
 			left:  nil,
-			right: nil}}},
-		t2: &Tree{root: &Node{val: 1,
-			right: &Node{val: 2,
+			right: nil}},
+		t2: &Tree{parent: nil,
+			right: &Tree{val: 2,
 				left:  nil,
 				right: nil},
-			left: &Node{val: 3,
-				left: &Node{val: 5,
+			left: &Tree{val: 3,
+				left: &Tree{val: 5,
 					left:  nil,
 					right: nil},
-				right: nil}}}, out: false},
-	{t1: &Tree{root: &Node{val: 1,
-		left: &Node{val: 2,
+				right: nil}}, out: false},
+	{t1: &Tree{parent: nil,
+		left: &Tree{val: 2,
 			left:  nil,
 			right: nil},
-		right: &Node{val: 3,
-			left: &Node{val: 4,
-				left:  &Node{val: 6, left: nil, right: nil},
-				right: &Node{val: 5, left: nil, right: nil},
+		right: &Tree{val: 3,
+			left: &Tree{val: 4,
+				left:  &Tree{val: 6, left: nil, right: nil},
+				right: &Tree{val: 5, left: nil, right: nil},
 			},
-			right: &Node{val: 7,
-				left:  &Node{val: 8, left: nil, right: nil},
-				right: nil}}}},
-		t2: &Tree{root: &Node{val: 1,
-			right: &Node{val: 2,
+			right: &Tree{val: 7,
+				left:  &Tree{val: 8, left: nil, right: nil},
+				right: nil}}},
+		t2: &Tree{parent: nil,
+			right: &Tree{val: 2,
 				left:  nil,
 				right: nil},
-			left: &Node{val: 3,
-				right: &Node{val: 4,
-					right: &Node{val: 6, left: nil, right: nil},
-					left:  &Node{val: 5, left: nil, right: nil},
+			left: &Tree{val: 3,
+				right: &Tree{val: 4,
+					right: &Tree{val: 6, left: nil, right: nil},
+					left:  &Tree{val: 5, left: nil, right: nil},
 				},
-				left: &Node{val: 7,
-					right: &Node{val: 8, left: nil, right: nil},
-					left:  nil}}}},
+				left: &Tree{val: 7,
+					right: &Tree{val: 8, left: nil, right: nil},
+					left:  nil}}},
 		out: true},
+}
+
+func TestTreeDelete(t *testing.T) {
+	data := []struct {
+		vals []int
+		del  int
+		str  string
+	}{
+		// case del leaf
+		{[]int{2, 1, 7, 4, 3, 6, 5, 8}, 3, "2174865"},
+		// case del subtree with one child
+		{[]int{2, 1, 7, 4, 3, 6, 5, 8}, 6, "2174835"},
+		// case del subtree with both children
+		{[]int{2, 1, 7, 4, 3, 6, 5, 8}, 4, "2175836"},
+	}
+
+	for i, d := range data {
+		tree := &Tree{val: d.vals[0]}
+		for j := 1; j < len(d.vals); j++ {
+			Insert(&tree, d.vals[j])
+		}
+		Delete(&tree, d.del)
+		out := tree.ToString()
+		if out != d.str {
+			t.Errorf("case [%v] expected %v, got %v", i, d.str, out)
+		}
+
+	}
+}
+func TestTreeInsert(t *testing.T) {
+	data := []struct {
+		vals []int
+		str  string
+	}{
+		{[]int{1, 2, 3, 4}, "1234"},
+		{[]int{4, 2, 3, 4}, "4243"},
+	}
+
+	for i, d := range data {
+		tree := &Tree{val: d.vals[0]}
+		for j := 1; j < len(d.vals); j++ {
+			Insert(&tree, d.vals[j])
+		}
+		out := tree.ToString()
+		if out != d.str {
+			t.Errorf("case [%v] expected %v, got %v", i, d.str, out)
+		}
+
+	}
+}
+
+func TestTreeFindMax(t *testing.T) {
+	data := []struct {
+		vals []int
+		max  int
+	}{
+		{[]int{1, 2, 3, 4}, 4},
+		{[]int{4, 2, 3, 4, 10}, 10},
+	}
+
+	for i, d := range data {
+		tree := &Tree{val: d.vals[0]}
+		for j := 1; j < len(d.vals); j++ {
+			Insert(&tree, d.vals[j])
+		}
+		out := tree.FindMax()
+		if out != d.max {
+			t.Errorf("case [%v] expected %v, got %v", i, d.max, out)
+		}
+
+	}
+}
+func TestTreeFindMin(t *testing.T) {
+	data := []struct {
+		vals []int
+		min  int
+	}{
+		{[]int{1, 2, 3, 4}, 1},
+		{[]int{4, 2, 3, 4, 10}, 2},
+	}
+
+	for i, d := range data {
+		tree := &Tree{val: d.vals[0]}
+		for j := 1; j < len(d.vals); j++ {
+			Insert(&tree, d.vals[j])
+		}
+		out := tree.FindMin()
+		if out != d.min {
+			t.Errorf("case [%v] expected %v, got %v", i, d.min, out)
+		}
+
+	}
 }
 
 func TestTreeIsMirror(t *testing.T) {
@@ -76,51 +168,62 @@ func TestTreeReverse(t *testing.T) {
 		out string
 	}{
 		{in: nil, out: "empty"},
-		{in: &Tree{root: &Node{val: 1,
-			left: &Node{val: 2,
-				left:  nil,
-				right: nil},
-			right: &Node{val: 3,
-				left: &Node{val: 4,
-					left:  &Node{val: 6, left: nil, right: nil},
-					right: &Node{val: 5, left: nil, right: nil},
+		{in: &Tree{parent: nil,
+			val:  1,
+			left: &Tree{val: 2},
+			right: &Tree{val: 3,
+				left: &Tree{val: 4,
+					left:  &Tree{val: 6},
+					right: &Tree{val: 5},
 				},
-				right: &Node{val: 7,
-					left:  &Node{val: 8, left: nil, right: nil},
-					right: nil}}}}, out: "13274856"},
+				right: &Tree{val: 7,
+					left:  &Tree{val: 8},
+					right: nil}}}, out: "13274856"},
 	}
 
 	for i, d := range data {
 		d.in.Reverse()
-		if d.in.String() != d.out {
-			t.Errorf("case [%v] expected %v, got %v", i, d.out, d.in.String())
+		if d.in.ToString() != d.out {
+			t.Errorf("case [%v] expected %v, got %v", i, d.out, d.in.ToString())
 		}
 	}
 }
 
-func TestTreeString(t *testing.T) {
+func TestTreeToString(t *testing.T) {
 	data := []struct {
 		in  *Tree
 		out string
 	}{
 		{in: nil, out: "empty"},
-		{in: &Tree{root: &Node{val: 1,
-			left: &Node{val: 2,
-				left:  nil,
-				right: nil},
-			right: &Node{val: 3,
-				left: &Node{val: 4,
-					left:  &Node{val: 6, left: nil, right: nil},
-					right: &Node{val: 5, left: nil, right: nil},
+		{in: &Tree{parent: nil,
+			val:  1,
+			left: &Tree{val: 2},
+			right: &Tree{val: 3,
+				left: &Tree{val: 4,
+					left:  &Tree{val: 6},
+					right: &Tree{val: 5},
 				},
-				right: &Node{val: 7,
-					left:  &Node{val: 8, left: nil, right: nil},
-					right: nil}}}}, out: "12347658"},
+				right: &Tree{val: 7,
+					left:  &Tree{val: 8},
+					right: nil}}}, out: "12347658"},
+		{in: &Tree{
+			val: 4,
+			left: &Tree{
+				val:  2,
+				left: nil,
+				right: &Tree{
+					val:   3,
+					left:  nil,
+					right: &Tree{val: 4},
+				},
+			},
+			right: nil,
+		}, out: "4234"},
 	}
 
 	for i, d := range data {
-		if d.in.String() != d.out {
-			t.Errorf("case [%v] expected %v, got %v", i, d.out, d.in.String())
+		if d.in.ToString() != d.out {
+			t.Errorf("case [%v] expected %v, got %v", i, d.out, d.in.ToString())
 		}
 	}
 }
