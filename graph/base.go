37aebefa3772dc8f7cdc943e9a2c6723e688c8ef
Mirolim Mirzakhmedov <mirolim.mirzakhmedov@gmail.com>
2019-07-10 17:43:46 +0500

    add graph Bipartite (two coloring)

--- graph/base.go
+++ graph/base.go
@@ -291,19 +291,63 @@ func (g *Graph) isSameEdge(v1, v2 int, m map[int]map[int]bool) bool {
 func (g *Graph) Components() (components [][]int) {
 	discovered := make([]bool, g.numOfVertices+1)
 	var component []int
-	processEarly := func(v int) {
+	addToComponent := func(v int) {
 		discovered[v] = true
 		component = append(component, v)
 	}
 	for i := 1; i <= g.numOfVertices; i++ {
 		if !discovered[i] {
-			g.BFS(i, processEarly, nil, nil, nil)
+			g.BFS(i, addToComponent, nil, nil, nil)
 			components = append(components, make([]int, len(component)))
 			copy(components[len(components)-1][0:], component)
 			component = component[:0]
 		}
 	}
 	return
+}
+
+func (g *Graph) Bipartite() ([2][]int, bool) {
+	var out [2][]int
+	bipartite := true
+	uncolored, white, black := 0, 1, 2
+	discovered := make([]bool, g.numOfVertices+1)
+	color := make([]int, g.numOfVertices+1)
+	complement := func(c int) int {
+		if c == white {
+			return black
+		} else if c == black {
+			return white
+		}
+		return uncolored
+	}
+	processEarly := func(v int) {
+		discovered[v] = true
+	}
+	processEdge := func(v1, v2 int) {
+		if color[v1] == color[v2] {
+			// same color
+			bipartite = false
+		}
+		color[v2] = complement(color[v1])
+	}
+	for i := 1; i <= g.numOfVertices; i++ {
+		if !discovered[i] {
+			color[i] = white
+			g.BFS(i, processEarly, nil, processEdge, nil)
+			if !bipartite {
+				break
+			}
+		}
+	}
+	if bipartite {
+		for i := 1; i <= g.numOfVertices; i++ {
+			if color[i] == uncolored {
+				panic("uncolored found")
+			}
+			out[color[i]-1] = append(out[color[i]-1], i)
+		}
+	}
+	return out, bipartite
 }
 
 func (g *Graph) ToString() string {
