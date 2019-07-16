1e09ee71a704f890efa708b4b67206b2a4bd0420
Mirolim Mirzakhmedov <mirolim.mirzakhmedov@gmail.com>
2019-07-16 18:44:00 +0500

    move graph Component, Bipartite and FindPath to properties file

--- graph/properties.go
+++ graph/properties.go
@@ -27,3 +27,84 @@ func (g *Graph) FindCycle(start int) ([]int, error) {
 	}
 	return nil, err
 }
+
+// TODO handle directed graphs
+func (g *Graph) Components() (components [][]int) {
+	discovered := make([]bool, g.numOfVertices+1)
+	var component []int
+	addToComponent := func(v int) {
+		discovered[v] = true
+		component = append(component, v)
+	}
+	for i := 1; i <= g.numOfVertices; i++ {
+		if !discovered[i] {
+			g.BFS(i, addToComponent, nil, nil, nil)
+			components = append(components, make([]int, len(component)))
+			copy(components[len(components)-1][0:], component)
+			component = component[:0]
+		}
+	}
+	return
+}
+
+func (g *Graph) Bipartite() ([2][]int, bool) {
+	var out [2][]int
+	bipartite := true
+	uncolored, white, black := 0, 1, 2
+	discovered := make([]bool, g.numOfVertices+1)
+	color := make([]int, g.numOfVertices+1)
+	complement := func(c int) int {
+		if c == white {
+			return black
+		} else if c == black {
+			return white
+		}
+		return uncolored
+	}
+	processEarly := func(v int) {
+		discovered[v] = true
+	}
+	processEdge := func(v1, v2 int) {
+		if color[v1] == color[v2] {
+			// same color
+			bipartite = false
+		}
+		color[v2] = complement(color[v1])
+	}
+	for i := 1; i <= g.numOfVertices; i++ {
+		if !discovered[i] {
+			color[i] = white
+			g.BFS(i, processEarly, nil, processEdge, nil)
+			if !bipartite {
+				break
+			}
+		}
+	}
+	if bipartite {
+		for i := 1; i <= g.numOfVertices; i++ {
+			if color[i] == uncolored {
+				panic("uncolored found")
+			}
+			out[color[i]-1] = append(out[color[i]-1], i)
+		}
+	}
+	return out, bipartite
+}
+
+func FindPath(start, end int, parent []int) ([]int, error) {
+	var path []int
+	if end > len(parent)-1 {
+		return nil, errors.New("no end vertices in path")
+	}
+	count := 0
+	for start != end {
+		path = append(path, end)
+		end = parent[end]
+		count++
+		if count > len(parent) {
+			return nil, errors.New("path not found")
+		}
+	}
+	path = append(path, start)
+	return path, nil
+}
