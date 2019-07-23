feed5e2ef86099f064718d46d2300188a9b873e0
Mirolim Mirzakhmedov <mirolim.mirzakhmedov@gmail.com>
2019-07-24 01:40:54 +0500

    solution to PC The Tourist Guide problem

---
+++ graph/the_tourist_guide_test.go
@@ -0,0 +1,124 @@
+package graph
+
+import (
+	"testing"
+
+	"github.com/mmirolim/algos/checks"
+)
+
+/*
+9.6.3 The Tourist Guide
+PC/UVa IDs: 110903/10099, Popularity: B, Success rate: average Level: 3
+*/
+func TestTouristGuide(t *testing.T) {
+	cityMap := `7 10 weighted
+1 2 30
+1 3 15
+1 4 10
+2 4 25
+2 5 60
+3 4 40
+3 6 20
+4 7 35
+5 7 20
+6 7 30
+`
+	data := []struct {
+		start, end    int
+		cityMap       string
+		numOfTourists int
+		numOfTrips    int
+	}{
+		{start: 1, end: 7, cityMap: cityMap, numOfTourists: 99, numOfTrips: 5},
+	}
+	for i, d := range data {
+		out := TheTouristGuide(d.start, d.end, d.numOfTourists, d.cityMap)
+		checks.AssertEq(t, d.numOfTrips, out, caseStr(i))
+	}
+}
+
+func TheTouristGuide(start, end, numOfTourist int, cityMap string) int {
+	var paths [][]int
+	cityGraph, err := NewGraphFrom(cityMap)
+	if err != nil {
+		panic(err)
+	}
+	discovered := make([]bool, cityGraph.NumOfVertices()+1)
+	discovered[start] = true
+	resetDiscovered := func() {
+		for i := range discovered {
+			discovered[i] = false
+		}
+		discovered[start] = true
+	}
+	var stack []int
+	storePath := func() {
+		var path []int
+		path = append(path, start)
+		for i := range stack {
+			path = append(path, stack[i])
+		}
+		paths = append(paths, path)
+	}
+
+	var dfs func(v int)
+	dfs = func(start int) {
+		push(&stack, start)
+		discovered[start] = true
+	OUTER:
+		for len(stack) > 0 {
+			cur := peek(&stack)
+			if cur == end {
+				storePath()
+				// pop end from stack
+				pop(&stack)
+				continue
+			}
+			for _, v := range cityGraph.edges[cur].ids() {
+				if !discovered[v] {
+					discovered[v] = true
+					push(&stack, v)
+					continue OUTER
+				}
+			}
+			pop(&stack)
+			discovered[end] = false
+		}
+	}
+
+	for _, v := range cityGraph.edges[start].ids() {
+		dfs(v)
+		resetDiscovered()
+	}
+
+	edges := cityGraph.EdgesWithWeights()
+	maxMin := 0
+	minTrips := -1
+	findWeight := func(v1, v2 int) int {
+		for _, e := range edges {
+			if (e[0] == v1 && e[1] == v2) || (e[0] == v2 && e[1] == v1) {
+				return e[2]
+			}
+		}
+		panic("edge not found")
+	}
+	for _, p := range paths {
+		min := 1 << 30
+		for i := 0; i < len(p)-1; i++ {
+			w := findWeight(p[i], p[i+1])
+			if min > w {
+				min = w
+			}
+		}
+		if maxMin < min {
+			maxMin = min
+			if numOfTourist%(maxMin-1) == 0 {
+				minTrips = numOfTourist / (maxMin - 1)
+			} else {
+				// add one more trip if some tourist left
+				minTrips = 1 + numOfTourist/(maxMin-1)
+			}
+		}
+	}
+	return minTrips
+}
