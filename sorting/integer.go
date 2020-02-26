817d0f588ddf5b5fcd86fb953b194e0301bd858c
Mirolim Mirzakhmedov <mirolim.mirzakhmedov@gmail.com>
2020-02-26 17:15:00 +0500

    use insertion sort as small sort in mapRangeSort, bench with radix sort

--- sorting/integer.go
+++ sorting/integer.go
@@ -1,7 +1,10 @@
 package sorting
 
-import "sync"
+import (
+	"sync"
+)
 
+// similar to flashsort
 func mapRangeParams(arr []int) (a1, da, size int, sorted bool) {
 	min := arr[0]
 	max := arr[0]
@@ -50,6 +53,9 @@ func mapRange(arr []int, out []int) []int {
 		arr[0], arr[1] = arr[1], arr[0]
 		return arr
 	}
+	if len(arr) < 8 {
+		return InsertionSort(arr)
+	}
 	a1, da, size, sorted := mapRangeParams(arr)
 	if sorted {
 		return arr
@@ -89,10 +95,88 @@ func mapRange(arr []int, out []int) []int {
 	return arr
 }
 
+func mapRangeParallel(arr []int, out []int) []int {
+	if len(arr) == 2 {
+		if arr[0] <= arr[1] {
+			return arr
+		}
+		// swap
+		arr[0], arr[1] = arr[1], arr[0]
+		return arr
+	}
+	if len(arr) < 8 {
+		return InsertionSort(arr)
+	}
+	a1, da, size, sorted := mapRangeParams(arr)
+	if sorted {
+		return arr
+	}
+	if out == nil {
+		out = make([]int, len(arr))
+	} else {
+		out = out[:len(arr)]
+	}
+	t := 0
+	for i := range arr {
+		t = (arr[i] - a1) * size / da
+		out[i] = t
+	}
+	sorted = true
+	prev := out[0]
+	for _, d := range out {
+		if prev >= d {
+			sorted = false
+			break
+		}
+		prev = d
+	}
+	buckets := pool.Alloc(len(arr))
+	for i := range out {
+		buckets[out[i]] = append(buckets[out[i]], arr[i])
+	}
+	j := 0
+	var wg sync.WaitGroup
+	for i := range buckets {
+		if len(buckets[i]) == 1 {
+			arr[j] = buckets[i][0]
+			j++
+		} else if len(buckets[i]) > 1 {
+			if len(buckets[i]) > 8 {
+				wg.Add(1)
+				go func(bid, aid int) {
+					copy(arr[aid:], mapRange(buckets[bid], out))
+					wg.Done()
+				}(i, j)
+
+			} else {
+				copy(arr[j:], mapRange(buckets[i], out))
+			}
+			j += len(buckets[i])
+		}
+	}
+	wg.Wait()
+	pool.Free(buckets)
+	return arr
+}
+
 func SortIntegers(arr []int) []int {
 	return arr
 }
 
 func isNaN(a float64) bool {
 	return a != a
+}
+
+func InsertionSort(arr []int) []int {
+	n := len(arr)
+	for i := 1; i < n; i++ {
+		val := arr[i]
+		j := i
+		for j > 0 && val < arr[j-1] {
+			arr[j], arr[j-1] = arr[j-1], arr[j]
+			j--
+		}
+		arr[j] = val
+	}
+	return arr
 }
--- sorting/integer_test.go
+++ sorting/integer_test.go
@@ -6,6 +6,8 @@ import (
 	"sort"
 	"testing"
 	"time"
+
+	"github.com/shawnsmithdev/zermelo/zint"
 )
 
 func TestMapRangeSort(t *testing.T) {
@@ -29,12 +31,33 @@ func TestMapRangeSort(t *testing.T) {
 	}
 }
 
+func TestMapRangeSortParallel(t *testing.T) {
+	data := []struct {
+		in []int
+	}{
+		{[]int{2, 1, 5, 8, 0}},
+		{[]int{0, 10, 99, 98, 2}},
+		{[]int{1000, 2, 2, 99, 99, 1, 50}},
+		{[]int{74, 59, 238, -784, 9845, 959, 905, 0, 0, 42, 7586, -5467984, 7586}},
+		{rand.Perm(10000)[:1000]},
+	}
+
+	for i, d := range data {
+		out := mapRangeParallel(d.in, nil)
+		if !isSorted(out) {
+			t.Errorf("case [%v] expected sorted got %v", i, out)
+		}
+	}
+}
+
 func TestPerfOfRangeSortVSStdStort(t *testing.T) {
-	max := 100000000
-	n := 10000000
+	max := 1000000
+	n := max / 10
 	start := time.Now()
 	randA100 := rand.Perm(max)[:n]
 	randB100 := rand.Perm(max)[:n]
+	randC100 := rand.Perm(max)[:n]
+	randD100 := rand.Perm(max)[:n]
 	fmt.Printf("Rand arr generation time %+v\n", time.Since(start)) // output for debug
 
 	start = time.Now()
@@ -43,9 +66,17 @@ func TestPerfOfRangeSortVSStdStort(t *testing.T) {
 	if !isSorted(out) {
 		t.Errorf("case [%s] expected sorted got %v", "mapRange", out)
 	}
+	out = mapRangeParallel(randC100, nil)
+	fmt.Printf("rangeSortParallel time\n%+v for max %d and n %d\n", time.Since(start), max, n) // output for debug
+	if !isSorted(out) {
+		t.Errorf("case [%s] expected sorted got %v", "mapRange", out)
+	}
 	start = time.Now()
 	sort.Ints(randB100)
 	fmt.Printf("sort.Ints time\n%+v for max %d and n %d\n", time.Since(start), max, n)
+	start = time.Now()
+	zint.Sort(randD100)
+	fmt.Printf("zint.Sort time\n%+v for max %d and n %d\n", time.Since(start), max, n)
 }
 
 func isSorted(arr []int) bool {
@@ -63,7 +94,7 @@ func BenchmarkMapRangeSortRand100(b *testing.B) {
 	b.StopTimer()
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		rand100 := rand.Perm(10000)[:100]
+		rand100 := rand.Perm(10000)[:1000]
 		b.StartTimer()
 		_ = mapRange(rand100, nil)
 		b.StopTimer()
@@ -74,9 +105,20 @@ func BenchmarkMapRangeSortStdSortIntsRand100(b *testing.B) {
 	b.StopTimer()
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		rand100 := rand.Perm(10000)[:100]
+		rand100 := rand.Perm(10000)[:1000]
 		b.StartTimer()
 		sort.Ints(rand100)
+		b.StopTimer()
+	}
+}
+
+func BenchmarkMapRangeSortZIntsRand100(b *testing.B) {
+	b.StopTimer()
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		rand100 := rand.Perm(10000)[:1000]
+		b.StartTimer()
+		zint.Sort(rand100)
 		b.StopTimer()
 	}
 }
@@ -105,6 +147,20 @@ func BenchmarkSortInt64K(b *testing.B) {
 		}
 		b.StartTimer()
 		sort.Ints(data)
+		b.StopTimer()
+	}
+}
+
+func BenchmarkSortRadixInt64K(b *testing.B) {
+	b.ReportAllocs()
+	b.StopTimer()
+	for i := 0; i < b.N; i++ {
+		data := make([]int, 1<<16)
+		for i := 0; i < len(data); i++ {
+			data[i] = i ^ 0xcccc
+		}
+		b.StartTimer()
+		zint.Sort(data)
 		b.StopTimer()
 	}
 }
